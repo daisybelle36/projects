5b92168955e0a36aba255b8a68154e9bec0aaece
Tania Strahan <thraell17@gmail.com>
2023-07-30 18:14:47 +1000

    make script work on multiple files

--- whisper/transcribe_file.py
+++ whisper/transcribe_file.py
@@ -3,55 +3,41 @@
 # --- LIBRARIES --- #
 from argparse import ArgumentParser
 import whisper
-#import openai
-#import openai_whisper
+import glob
  
 # --- VARIABLES --- #
-#HEAD_INDEX = 0
-#PRON_INDEX = 1
  
 # --- OBJECTS ----- #
-#class Entry():
-#def __init__(self, head, pron, rank):
-#self.head = head
-#self.pron = pron
-#self.rank = rank
  
 # --- HELPER FUNCTIONS --- #
-# none yet
  
 # --- MAIN --------------- #
 def main():
 	# create parser
 	parser = ArgumentParser(description = 'transcribe an audio file using Whisper')
 	# add arguments to parser
-	parser.add_argument('audio_file', help='.mp3 file, etc')
-	#parser.add_argument('-w', '--word', help='col of headword, default = 1', default = 1)
-	# collate/parse arguments
+	parser.add_argument('path', nargs='+', help='Path of a file or a folder of files.')
+	# collate/parse arguments, including glob
 	args = parser.parse_args()
-	print(f'{args=}')
- 	
-	# read in audio
-	prefix = args.audio_file[:-4]
-	print(f'{prefix=}')
-	audio_input = args.audio_file
-	print(f'{audio_input=}')
- 		
-	# transcribe audio
+	# glob.glob -> list; glob.iglob -> iterator
+	globbed_paths = sorted(glob.iglob(args.path[0]))
+
+	# load model
 	model = whisper.load_model("base")
 	print("'base' model loaded")
-	#result = model.transcribe("audio_input")
-	result = model.transcribe(args.audio_file, fp16=False)
-	print(result)
-	for segment in result["segments"]:
-		print(segment["start"], segment["text"])
 
-	#with open(audio, 'r') as inFile:
-		#for line in inFile:
-			#currentAudio.append(line.strip())
-			#inFile.close()
- 	
-	#print(currentAudio)
+	for g in globbed_paths:
+		# read in audio
+		audio_input = g
+ 		
+		# transcribe audio
+		#verbose=False
+		result = model.transcribe(g, fp16=False)
+		#print(result)   # to see structure of transcribed text
+		# print transcribed text
+		for segment in result["segments"]:
+			print(g, round(segment["start"]), segment["text"], sep='\t')
+
  
 if __name__ == "__main__":
 	main()
